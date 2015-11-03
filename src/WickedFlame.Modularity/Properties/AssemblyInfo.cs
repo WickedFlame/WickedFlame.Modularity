f05815fb642787a0785953957f78db32efea3779
chriswalpen <c.walpen@gmx.net>
2015-11-03 22:21:39 +0100

    Corrected Nuspec description

--- src/WickedFlame.Modularity/Properties/AssemblyInfo.cs
+++ src/WickedFlame.Modularity/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WickedFlame.Modularity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("WickedFlame")]
 [assembly: AssemblyProduct("WickedFlame.Modularity")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.2")]
+[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.2.1")]
